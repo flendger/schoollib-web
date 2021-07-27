ffe9c9b94eeb01e6333859bb3b55d2e5764b9cee
flendger <kiru.ee@yandex.ru>
2021-07-27 12:48:27 +0300

    Spring initialization

---
+++ src/main/java/org/educationfree/schoollibweb/SchoollibWebApplication.java
@@ -0,0 +1,13 @@
+package org.educationfree.schoollibweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoollibWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoollibWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/educationfree/schoollibweb/SchoollibWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.educationfree.schoollibweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoollibWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
