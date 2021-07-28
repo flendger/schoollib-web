0606c4965c18e7669e0c2cfd68338bf4aac5e408
flendger <kiru.ee@yandex.ru>
2021-07-28 22:54:47 +0300

    Добавлен каталог учебных предметов

---
+++ src/main/java/org/educationfree/schoollibweb/model/catalog/Subject.java
@@ -0,0 +1,14 @@
+package org.educationfree.schoollibweb.model.catalog;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subjects")
+@Getter
+@Setter
+public class Subject extends BaseCatalog{
+}
