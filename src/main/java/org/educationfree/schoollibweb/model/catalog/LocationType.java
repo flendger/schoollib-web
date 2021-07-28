96a539cf54eae5bab68e43993df9c33bd0bca43b
flendger <kiru.ee@yandex.ru>
2021-07-28 22:44:49 +0300

    Добавлен каталог типов мест хранения

---
+++ src/main/java/org/educationfree/schoollibweb/model/catalog/LocationType.java
@@ -0,0 +1,14 @@
+package org.educationfree.schoollibweb.model.catalog;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "location_types")
+@Getter
+@Setter
+public class LocationType extends BaseCatalog{
+}
