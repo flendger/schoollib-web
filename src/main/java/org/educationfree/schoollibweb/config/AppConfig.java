3ae23584bfa73902936cee603b1957b5e3b59307
Raizel2134 <44531067+Raizel2134@users.noreply.github.com>
2021-07-30 12:53:30 +0300

    Добавил шаблон главной страницы, добавил туда заглушку в виде таблиц. Все что дублируется вынес в отдельные файлы и подключаю через th:replace.  Пока что добавил абсолютно все файлы для стилей и скриптов, добавил resourcesHandler для адекватного подтягивания стилей скриптов.

---
+++ src/main/java/org/educationfree/schoollibweb/config/AppConfig.java
@@ -0,0 +1,16 @@
+package org.educationfree.schoollibweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/static/**")) {
+            registry.addResourceHandler("/static/dist/**").addResourceLocations("classpath:/static/dist/");
+            registry.addResourceHandler("/static/vendors/**").addResourceLocations("classpath:/static/vendors/");
+        }
+    }
+}
