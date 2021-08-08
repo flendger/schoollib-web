2e2a9f42c49a7daea1e5cacbaab76430b99150e9
flendger <kiru.ee@yandex.ru>
2021-08-08 18:20:21 +0300

    Подключение возможности запуска на выделенном сервере приложений

--- src/main/java/org/educationfree/schoollibweb/SchoollibWebApplication.java
+++ src/main/java/org/educationfree/schoollibweb/SchoollibWebApplication.java
@@ -2,12 +2,18 @@ package org.educationfree.schoollibweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SchoollibWebApplication {
+public class SchoollibWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SchoollibWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoollibWebApplication.class, args);
 	}
-
 }
