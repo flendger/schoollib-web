f638ca45a1898d1c68f31769c37eeb07e747465d
flendger <kiru.ee@yandex.ru>
2021-08-12 16:45:57 +0300

    В инвентаризацию добавлено поле Место хранения

--- src/main/java/org/educationfree/schoollibweb/model/operation/InventionOperation.java
+++ src/main/java/org/educationfree/schoollibweb/model/operation/InventionOperation.java
@@ -2,12 +2,10 @@ package org.educationfree.schoollibweb.model.operation;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.educationfree.schoollibweb.model.catalog.Location;
 import org.educationfree.schoollibweb.model.operation.item.InventionItem;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -16,6 +14,10 @@ import java.util.List;
 public class InventionOperation extends AbstractOperation{
     @Column(name = "comment")
     private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 
     @OneToMany(mappedBy = "document", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<InventionItem> items;
