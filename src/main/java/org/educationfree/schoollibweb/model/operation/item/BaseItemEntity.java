48f852e5b38e01cb06c806531fea5d043c754718
flendger <kiru.ee@yandex.ru>
2021-08-12 16:18:01 +0300

    Добавлены абстрактные классы операций и строк операций

---
+++ src/main/java/org/educationfree/schoollibweb/model/operation/item/BaseItemEntity.java
@@ -0,0 +1,24 @@
+package org.educationfree.schoollibweb.model.operation.item;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.educationfree.schoollibweb.model.operation.AbstractOperation;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseItemEntity<T extends AbstractOperation> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "row")
+    private int row;
+
+    @ManyToOne
+    @JoinColumn(name = "doc_id")
+    private T document;
+}
