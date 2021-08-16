cd837747ec41e6fe2af899c42a7c3d00aa9c08dc
flendger <kiru.ee@yandex.ru>
2021-08-16 21:51:37 +0300

    Реализован InventionItemService

--- src/test/java/org/educationfree/schoollibweb/service/operation/item/InventionItemServiceTest.java
+++ src/test/java/org/educationfree/schoollibweb/service/operation/item/InventionItemServiceTest.java
@@ -1,10 +1,13 @@
 package org.educationfree.schoollibweb.service.operation.item;
 
+import org.educationfree.schoollibweb.model.catalog.Book;
 import org.educationfree.schoollibweb.model.operation.Invention;
+import org.educationfree.schoollibweb.model.operation.item.InventionItem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +21,32 @@ class InventionItemServiceTest {
     void findLast() {
         Invention invention = new Invention();
         invention.setId(1L);
-        inventionItemService.findLast(invention);
-        inventionItemService.findAllByDocument(invention);
+        assertDoesNotThrow(() -> inventionItemService.findLast(invention));
+    }
+
+    @Test
+    void findAllByDocument() {
+        Invention invention = new Invention();
+        invention.setId(1L);
+        assertDoesNotThrow(() -> inventionItemService.findAllByDocument(invention));
+    }
+
+    @Test
+    @Transactional
+    void save() {
+        Invention invention = new Invention();
+        invention.setId(1L);
+
+        InventionItem newItem = inventionItemService.getNewItem(invention);
+
+        Book book = new Book();
+        book.setId(66L);
+        newItem.setBook(book);
+        newItem.setQuantity(55);
+
+        assertDoesNotThrow(() -> inventionItemService.save(newItem));
+        assertNotNull(newItem.getId());
+        assertNotNull(newItem.getRow());
+        System.out.println(newItem);
     }
 }
