007a36cb26cd4f8a80e047caa1cdf8448d38b5be
flendger <kiru.ee@yandex.ru>
2021-08-17 09:12:41 +0300

    Добавлены репозитории для операции Поступление от издательств

---
+++ src/main/java/org/educationfree/schoollibweb/repository/operation/model/ReceiptFromPublisherRepository.java
@@ -0,0 +1,9 @@
+package org.educationfree.schoollibweb.repository.operation.model;
+
+import org.educationfree.schoollibweb.model.operation.ReceiptFromPublisher;
+import org.educationfree.schoollibweb.repository.operation.OperationRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReceiptFromPublisherRepository extends OperationRepository<ReceiptFromPublisher> {
+}
