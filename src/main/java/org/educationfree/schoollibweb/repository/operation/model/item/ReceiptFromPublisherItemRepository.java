6a2be8b8ce2be5baaeefcadaca5af93bba66692f
flendger <kiru.ee@yandex.ru>
2021-08-18 10:58:01 +0300

    ReceiptFromPublisherItemService переведен на общий интерфейс OperationItemService

--- src/main/java/org/educationfree/schoollibweb/repository/operation/model/item/ReceiptFromPublisherItemRepository.java
+++ src/main/java/org/educationfree/schoollibweb/repository/operation/model/item/ReceiptFromPublisherItemRepository.java
@@ -1,16 +1,8 @@
 package org.educationfree.schoollibweb.repository.operation.model.item;
 
-import org.educationfree.schoollibweb.model.operation.ReceiptFromPublisher;
 import org.educationfree.schoollibweb.model.operation.item.ReceiptFromPublisherItem;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface ReceiptFromPublisherItemRepository extends JpaRepository<ReceiptFromPublisherItem, Long> {
-    Optional<ReceiptFromPublisherItem> findTopByDocumentOrderByRowDesc(ReceiptFromPublisher document);
-
-    List<ReceiptFromPublisherItem> findAllByDocument(ReceiptFromPublisher document);
+public interface ReceiptFromPublisherItemRepository extends OperationItemRepository<ReceiptFromPublisherItem> {
 }
--- src/main/java/org/educationfree/schoollibweb/service/operation/item/ReceiptFromPublisherItemService.java
+++ src/main/java/org/educationfree/schoollibweb/service/operation/item/ReceiptFromPublisherItemService.java
@@ -3,53 +3,25 @@ package org.educationfree.schoollibweb.service.operation.item;
 import lombok.RequiredArgsConstructor;
 import org.educationfree.schoollibweb.model.operation.ReceiptFromPublisher;
 import org.educationfree.schoollibweb.model.operation.item.ReceiptFromPublisherItem;
-import org.educationfree.schoollibweb.repository.operation.model.item.ReceiptFromPublisherItemRepository;
+import org.educationfree.schoollibweb.repository.operation.model.item.OperationItemRepository;
+import org.educationfree.schoollibweb.service.operation.OperationService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class ReceiptFromPublisherItemService {
-    private final ReceiptFromPublisherItemRepository receiptFromPublisherItemRepository;
+public class ReceiptFromPublisherItemService extends AbstractOperationItemService<ReceiptFromPublisherItem>{
+    private final OperationItemRepository<ReceiptFromPublisherItem> receiptFromPublisherItemRepository;
+    private final OperationService<ReceiptFromPublisher> operationService;
 
-    public Optional<ReceiptFromPublisherItem> findLast(ReceiptFromPublisher document) {
-        return receiptFromPublisherItemRepository.findTopByDocumentOrderByRowDesc(document);
+    @Override
+    protected OperationItemRepository<ReceiptFromPublisherItem> getEntityRepository() {
+        return receiptFromPublisherItemRepository;
     }
 
-    public List<ReceiptFromPublisherItem> findAllByDocument(ReceiptFromPublisher document) {
-        return receiptFromPublisherItemRepository.findAllByDocument(document);
-    }
-
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public void save(ReceiptFromPublisherItem item) {
-        if (item == null) {
-            throw new IllegalArgumentException("Item cannot be null");
-        }
-
-        if (item.getRow() == null) {
-            item.setRow(getNewRowNum(item.getDocument()));
-        }
-
-        receiptFromPublisherItemRepository.save(item);
-    }
-
-    public int getNewRowNum(ReceiptFromPublisher document) {
-        Optional<ReceiptFromPublisherItem> lastItemOptional = findLast(document);
-        if (lastItemOptional.isPresent()) {
-            ReceiptFromPublisherItem lastItem = lastItemOptional.get();
-            return lastItem.getRow() + 1;
-        }
-        return 1;
-    }
-
-    public ReceiptFromPublisherItem getNewItem(ReceiptFromPublisher document) {
+    @Override
+    public ReceiptFromPublisherItem getNewItem(Long documentId) {
         ReceiptFromPublisherItem item = new ReceiptFromPublisherItem();
-        item.setDocument(document);
-
+        item.setDocument(operationService.getById(documentId));
         return item;
     }
 }
