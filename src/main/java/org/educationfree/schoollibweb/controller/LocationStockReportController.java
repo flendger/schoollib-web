6801448d54c7e0066f7696aa881611088b8f3c97
flendger <kiru.ee@yandex.ru>
2021-08-28 13:57:54 +0300

    Доработан вывод отчета

--- src/main/java/org/educationfree/schoollibweb/controller/LocationStockReportController.java
+++ src/main/java/org/educationfree/schoollibweb/controller/LocationStockReportController.java
@@ -1,15 +1,21 @@
 package org.educationfree.schoollibweb.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.educationfree.schoollibweb.service.report.LocationStockReportService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/location_stock_report")
+@RequiredArgsConstructor
 public class LocationStockReportController {
+    private final LocationStockReportService reportService;
 
     @GetMapping
-    public String getReport() {
+    public String getReport(Model model) {
+        model.addAttribute("report_rows", reportService.getRows());
         return "location_stock_report";
     }
 }
